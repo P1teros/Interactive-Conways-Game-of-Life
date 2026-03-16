97f0e5815c90cd38142ba1b869630f9095cffa5d
australopiter <164238849+P1teros@users.noreply.github.com>
2026-03-16 19:04:24 +0100

    Create Grid class with initialization and glider
    Initialize a grid with specified dimensions and a glider pattern.

---
+++ headers/grid.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+
+
+class Grid
+{
+    public:
+        Grid(int width, int height, int cSize)
+        {
+            rows = height / cSize;
+            columns = width / cSize;
+            cellSize = cSize; // zapisujemy rozmiar komorki
+            
+            //tworzenie i wypelnianie planszy
+            cells = std:: vector<std:: vector<int>>(rows, std:: vector<int>(columns,0)); 
+            //1. tworzy listę, która ma x miejsc (x = liczba kolumn) i na każdym miejscu przypisz 0
+            //2. bierze apsek z x zerami i "klonuje" go w dół o y razy (y = liczba wierszy)  
+
+            // TUTAJ DODAJEMY GLIDERA (Szybowiec)
+            cells[5][6] = 1;
+            cells[6][7] = 1;
+            cells[7][5] = 1;
+            cells[7][6] = 1;
+            cells[7][7] = 1;
+        }
+
+        void Draw();
+        void Update();
+        void SetValue(int row, int column, int value);
+        int CountLiveNeighbours(int row, int column);
+        int IsAlive(int row, int column);
+
+    private:
+        int rows;
+        int columns;
+        int cellSize;
+        std:: vector<std:: vector<int>> cells;
+
+};
